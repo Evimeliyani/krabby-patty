db178b6dab17acb6b40a9c833c185927b1afcffb
Evimeliyani <evimeliyani016@gmail.com>
2025-09-16 09:47:26 +0700

    inisiasi resep krabby patty

---
+++ Resep.java
@@ -0,0 +1,10 @@
+public class Resep {
+    public static void main (String[] args) {
+        // Example usage of the Resep class
+        System.out.println("Resep krabby patty");
+         System.out.println("----------------");
+          System.out.println("1. Bawang");
+           System.out.println("2. Ketimun");
+    }
+    
+}
