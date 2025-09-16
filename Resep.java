b8cc738e4977688510e29494844ecac77a7f4d72
Fadilah <papadfadilah501@gmail.com>
2025-09-16 11:44:47 +0700

    tambahan

--- Resep.java
+++ Resep.java
@@ -5,6 +5,8 @@ public class Resep {
          System.out.println("----------------");
           System.out.println("1. Bawang");
            System.out.println("2. Ketimun");
+           System.out.println("3. Tomat");
+           System.out.println("4. Cabe");
     }
     
 }
